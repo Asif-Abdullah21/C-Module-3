ba166eab64d58103be1112215762739c8772f3f5
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-04 23:28:55 +0600

    LOop

--- Loop.c
+++ Loop.c
@@ -58,15 +58,31 @@
 // }
 
 
+// #include<stdio.h>
+// int main()
+// {
+//     int i,n;
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++)//i++-> i=i+1
+//     {
+//         if(i%2==0) printf("%d - Even\n",i);
+//         else printf("%d - Odd\n",i);
+//     }
+
+// }
+
 #include<stdio.h>
 int main()
 {
-    int i,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)//i++-> i=i+1
+    int i;
+    for(i=1;i<=10;i++)//i++-> i=i+1
     {
-        if(i%2==0) printf("%d - Even\n",i);
-        else printf("%d - Odd\n",i);
+        printf("%d\n",i);
+        
+        if(i==5)
+        {
+            break;
+        }
     }
 
 }
