063241f0f64ccebc49c8eb388acda07a820882af
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-04 23:05:36 +0600

    loop

--- Loop.c
+++ Loop.c
@@ -20,11 +20,22 @@
   
 // }
 
+// #include<stdio.h>
+// int main()
+// {
+//     int i;//for loop
+//     for(i=1;i<=20;i=i+2)//i++-> i=i+1
+//     {
+//         printf("%d ",i);
+//     }
+  
+// }
+
 #include<stdio.h>
 int main()
 {
     int i;//for loop
-    for(i=1;i<=20;i=i+2)//i++-> i=i+1
+    for(int i=1;i<=20;i=i*2)//i++-> i=i+1
     {
         printf("%d ",i);
     }
